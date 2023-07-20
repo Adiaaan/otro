0655c88c3e84a1545bcee9e90ee0590599c737c1
Adiaaan <136658397+Adiaaan@users.noreply.github.com>
2023-07-19 21:53:08 -0500

    bot_logic.py
    hyhj

---
+++ bot_logic.py
@@ -0,0 +1,44 @@
+import random
+from discord.ext import commands
+
+#in progress--
+def mem(aslo):
+    imagen0 = random.choice('img1.jpg/images')
+
+    for i in range(aslo):
+        imagen0
+#in progress--
+
+
+def presentacion_es(something_length0):
+    elements0 = [":smiley: ",":smile:"]
+    consejo0 = "hola buen dia "
+    imagen0 = ""
+
+
+    for i in range(something_length0):
+        consejo0 += random.choice(elements0) + imagen0
+
+    return consejo0
+
+def random_es1(something_length1):
+    elements1 = ["Hablamos de contaminación cuando en un entorno ingresan elementos o sustancias que normalmente no deberían estar en él y que afectan el equilibrio del ecosistema.","contaminacion, osea que esperas que te responda? :satisfied:"]
+    consejo1 = "La contaminacion es "
+
+    for i in range(something_length1):
+        consejo1 += random.choice(elements1)
+
+    return consejo1
+
+
+def random_es2(something_length2):
+    elements2 = ["borrando tus correos de spam puede ser un ejemplo de como empezar este acto ","quiza puedas dejar de usar obejtos desechables "]
+    consejo2 = "es realmente complicado actualmente pero "
+    emoji2 = [":skull:",":smile:"]
+
+    for i in range(something_length2):
+        consejo2 += random.choice(elements2) + random.choice(emoji2)
+
+    return consejo2
+
+#isnt finished yet :D
